a8971163273e1f7c635f60b72363e905c72bbf78
Matthew Pineda <mpineda3247@gmail.com>
2019-02-16 03:18:40 -0800

    made form class, css is messed up though

--- hackuci/src/App.js
+++ hackuci/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// import MainForm from './MainForm.js'
+//import MainForm from './MainForm.js'
 
 class App extends Component {
   render() {
@@ -13,9 +13,37 @@ class App extends Component {
             How are you feeling?
           </h1>
           <h2>Tell us your mood and we will choose the best drake song for you!</h2>
-
+          <NameForm/>
         </header>
       </div>
+    );
+  }
+}
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A phrase was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} class="search-container">
+       <input type="text" value={this.state.value} onChange={this.handleChange} id="search-bar" placeholder="Your mood in one word"/>
+       <button type="submit"><img class="search-icon" src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png"/></button>
+     </form>
     );
   }
 }
