94a7ec034bb94bbe4aaf8e510e2e46cf2e635ade
guilleeh <guilleeh@uci.edu>
2019-02-16 15:26:19 -0800

    hotfix, master working properly

--- hackuci/index.py
+++ hackuci/index.py
@@ -1,28 +1,28 @@
-from flask import Flask,jsonify,json
+from flask import Flask, jsonify, json, request
 import sentiment
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET'])
 def index():
 	artist = 'Kendrick Lamar'
 	songs = ['BLOOD.', ' DNA', 'YAH', 'ELEMENT.', 'FEEL.', 'LOYALTY.', 'PRIDE',
 		'HUMBLE.', 'LUST.', 'LOVE.', 'XXX.', 'FEAR.', 'GOD.', 'DUCKWORTH']
 	song_polarity_list = []
+	if request.method == 'GET':
+		try:
+			for song in songs:
+				lyrics = sentiment.main(song, artist)
 
-	try:
-		for song in songs:
-			lyrics = sentiment.main(song, artist)
+				empDict = {
+				'Polarity': sentiment.text_to_sentiment(lyrics),
+				'Song': song
+				}
 
-			empDict = {
-			'Polarity': sentiment.text_to_sentiment(lyrics),
-			'Song': song
-			}
+				song_polarity_list.append(empDict)
 
-			song_polarity_list.append(empDict)
-
-		# convert to json data
-		# jsonStr = json.dumps(song_polarity_list)
-	except Exception as e: 
-		print(e)
+			# convert to json data
+			# jsonStr = json.dumps(song_polarity_list)
+		except Exception as e: 
+			print(e)
 
 	return jsonify(song_polarity_list)
--- hackuci/src/App.js
+++ hackuci/src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faSearch)
 
 //import MainForm from './MainForm.js'
 
 class App extends Component {
+  constructor(props) {
+        super(props);
+        this.state = {
+            songRecieved: false
+        };
+    }
+
+    myCallback = (dataFromForm) => {
+      this.setState({songRecieved: dataFromForm});
+    }
+
   render() {
     return (
       <div className="App">
@@ -13,12 +29,65 @@ class App extends Component {
             How are you feeling?
           </h1>
           <h2>Tell us your mood and we will choose the best drake song for you!</h2>
-          <NameForm/>
+          <NameForm songCallBack={this.myCallback} />
+
+          <SongDisplay isSongReturned={this.state.songRecieved}/>
         </header>
       </div>
     );
   }
 }
+
+
+function spotifySearchChain() {
+
+  let songName = "Nonstop", artist = "Drake";
+
+    songName = songName.split(" ")
+    let songArray = new Array();
+    for(var i = 0; i < songName.length; i++) {
+      songArray.push(songName[i]);
+      if( i != songName.length - 1) {
+        songArray.push(" ");
+      }
+    }
+
+    var spotifyChain = "";
+
+    for(var i = 0; i < songArray.length; i++) {
+      spotifyChain += songArray[i];
+      if(i < songArray.length - 1) {
+        spotifyChain += "%20";
+      }
+    }
+
+    return spotifyChain;
+
+}
+
+function SongVisual(props) {
+
+  // var spotify = ("https://open.spotify.com/search/results/" + spotifySearchChain());
+  //  document.getElementById("link").setAttribute("href",spotify);
+
+  return <div>
+      <h1> is the best song for you right now.</h1>
+      <h2>Listen to it now on <a href="spotify.com">Spotify</a></h2>
+    </div>;
+}
+
+function SongDisplay(props) {
+  const isSongReturned = props.isSongReturned;
+  if(isSongReturned) {
+    return <SongVisual/>;
+  }
+  else {
+   return <p></p>;
+  }
+}
+
+
+
 
 class NameForm extends React.Component {
   constructor(props) {
@@ -33,16 +102,27 @@ class NameForm extends React.Component {
     this.setState({value: event.target.value});
   }
 
-  handleSubmit(event) {
-    alert('A phrase was submitted: ' + this.state.value);
+  handleSubmit(event, props) {
+    if(this.state.value != '') {
+      alert('A phrase was submitted: ' + this.state.value);
+      const songSentAndRecieved = true;
+      this.props.songCallBack(songSentAndRecieved);
+
+    }
+    else {
+      alert('Please Enter a Valid Phrase.')
+    }
     event.preventDefault();
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit} class="search-container">
-       <input type="text" value={this.state.value} onChange={this.handleChange} id="search-bar" placeholder="Your mood in one word"/>
-       <button type="submit"><img class="search-icon" src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png"/></button>
+       <input type="text" value={this.state.value} onChange={this.handleChange}
+        id="search-bar" placeholder="Your mood in a word or phrase"/>
+       <button type="submit" class="search-icon">
+        <FontAwesomeIcon icon="search" />
+       </button>
      </form>
     );
   }
